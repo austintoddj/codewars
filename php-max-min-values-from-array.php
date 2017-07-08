2c3694702487ba7eaeb011c577bcd1d0ca62f76e
Todd Austin <austin.todd.j@gmail.com>
2017-07-08 15:24:45 -0500

    Added max/min php kata

---
+++ php-max-min-values-from-array.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Return the highest value from a given array.
+ *
+ * @param array $array
+ *
+ * @return string
+ */
+function maximum($array) {
+    
+    return max($array);
+}
+
+/**
+ * Return the lowest value from a given array.
+ *
+ * @param array $array
+ *
+ * @return string
+ */
+function minimum($array) {
+
+    return min($array);
+}
