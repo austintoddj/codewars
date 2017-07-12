e753bab294dfe56ba355c0651e73ed661ef91b5f
Todd Austin <austin.todd.j@gmail.com>
2017-07-12 08:02:04 -0500

    Added anchor removal bash kata

---
+++ bash-remove-anchor-from-url.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+#
+# Examples:
+#   returns 'www.codewars.com'
+#   removeUrlAnchor('www.codewars.com#about')
+#
+#   returns 'www.codewars.com?page=1'
+#   removeUrlAnchor('www.codewars.com?page=1')
+
+echo $1 | cut -f1 -d "#"
